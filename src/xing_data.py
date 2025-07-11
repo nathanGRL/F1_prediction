47958f970e2a51dcd2830bebca49ebc72f42e7d1
nathanGRL <nathan.grossmann97@gmail.com>
2025-07-11 16:07:43 +0200

    removing unginished python code to x data

--- src/xing_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-destination_file = "G:\My Drive\Portfolio\F1_Prediction\data"
-
-df = pd.read_csv(destination_file + "\kaggle_archive\circuits.csv")
-print(df.head())
-print(df.info())
